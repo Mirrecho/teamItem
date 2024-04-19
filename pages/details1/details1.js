0a53e39d14a9a9ecdcb387654bf1ac8cab76e10b
YY44ASF <1901409487@qq.com>
2024-04-19 09:04:50 +0800

    yy

--- pages/details1/details1.js
+++ pages/details1/details1.js
@@ -1,66 +1,19 @@
-// pages/details1/details1.js
+// page/component/list/list.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{},
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onReady:function(){
+    // 页面渲染完成
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  onShow:function(){
+    // 页面显示
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  onHide:function(){
+    // 页面隐藏
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onUnload:function(){
+    // 页面关闭
   }
 })
