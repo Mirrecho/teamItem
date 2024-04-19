94a073c4ac7de4f5bfcb575ed3bad9681dc1d9d5
taylor <1315554227@qq.com>
2024-04-19 10:01:39 +0800

    zh

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,66 +1,85 @@
-// pages/search/search.js
+let timeId = null;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    data: {
+        history: [],
+        hot: ['草莓蛋糕', '草莓糕点', '奶油蛋糕'],
+        result: [
+            {
+                id: 1,
+                url: '../details2/details2',
+                thumb: '/imgs/s4.png',
+                title: '草莓蛋糕 7寸',
+                price: 99
+            },
+            {
+                id: 2,
+                url: '../details2/details2',
+                thumb: '/imgs/s5.png',
+                title: '草莓糕点 100g',
+                price: 19
+            }
+        ],
+        showKeywords: false,
+        keywords: ['冷饮', '西瓜', '水果捞', '热饮'],
+        value: '',
+        showResult: false,
+    },
+    cancelSearch() {
+        this.setData({
+            showResult: false,
+            showKeywords: false,
+            value: ''
+        })
+    },
+    searchInput(e) {
+        if(!e.detail.value){
+            this.setData({
+                showKeywords: false
+            })
+        }else{
+            if(!this.data.showKeywords){
+                timeId && clearTimeout(timeId);
+                timeId = setTimeout(() => {
+                    this.setData({
+                        showKeywords: true
+                    })
+                }, 1000)
+            }
+        }
+    },
+    keywordHandle(e) {
+        const text = e.target.dataset.text;
+        this.setData({
+            value: text,
+            showKeywords: false,
+            showResult: true
+        })
+        this.historyHandle(text);
+    },
+    historyHandle(value) {
+        let history = this.data.history;
+        const idx = history.indexOf(value);
+        if (idx === -1) {
+            // 搜索记录只保留8个
+            if (history.length > 7) {
+                history.pop();
+            }
+        } else {
+            history.splice(idx, 1);
+        }
+        history.unshift(value);
+        wx.setStorageSync('history', JSON.stringify(history));
+        this.setData({
+            history
+        });
+    },
+    onLoad() {
+        const history = wx.getStorageSync('history');
+        if (history) {
+            this.setData({
+                history: JSON.parse(history)
+            })
+            console.log(this.data.history);
+        }
+    }
 })
