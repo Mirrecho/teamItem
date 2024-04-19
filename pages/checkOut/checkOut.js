8c1372025f07512a4e252301f66394fe53f53f1e
taylor <1315554227@qq.com>
2024-04-19 09:58:08 +0800

    zh

--- pages/checkOut/checkOut.js
+++ pages/checkOut/checkOut.js
@@ -1,66 +1,56 @@
-// pages/checkOut/checkOut.js
+// page/component/orders/orders.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+	data:{
+	  address:{},
+	  hasAddress: false,
+	  total:0,
+	  orders:[
+		  {id:1,title:'草莓糕点 100g',image:'../../imgs/s5.png',num:4,price:19},
+		  {id:2,title:'水果捞 100g',image:'../../imgs/s6.png',num:1,price:9.9}
+		]
+	},
+  
+	onReady() {
+	  this.getTotalPrice();
+	},
+	
+	onShow:function(){
+	  const self = this;
+	  wx.getStorage({
+		key:'address',
+		success(res) {
+		  self.setData({
+			address: res.data,
+			hasAddress: true
+		  })
+		}
+	  })
+	},
+  
+	/**
+	 * 计算总价
+	 */
+	getTotalPrice() {
+	  let orders = this.data.orders;
+	  let total = 0;
+	  for(let i = 0; i < orders.length; i++) {
+		total += orders[i].num * orders[i].price;
+	  }
+	  this.setData({
+		total: total
+	  })
+	},
+  
+	toPay() {
+	  wx.showModal({
+		title: '提示',
+		content: '本系统只做演示，支付系统已屏蔽',
+		text:'center',
+		complete() {
+		  wx.switchTab({
+			url: '/page/component/user/user'
+		  })
+		}
+	  })
+	}
+  })
