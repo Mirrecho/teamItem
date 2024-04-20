e5a531d53ed36108abd96afa92ed5141f2ce274a
1610037483 <1610037483@qq.com>
2024-04-20 19:36:11 +0800

    lcx

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,54 @@
-// pages/cart/cart.js
+// page/component/new-pages/cart/cart.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+    data: {
+      carts:[],               // 购物车列表
+      hasList:false,          // 列表是否有数据
+      totalPrice:0,           // 总价，初始为0
+      selectAllStatus:true,    // 全选状态，默认全选
+      obj:{
+          name:"hello"
+      }
+    },
+    onShow() {
+      this.setData({
+        hasList: true,
+        carts:[
+          {id:1,title:'草莓糕点 100g',image:'/imgs/s5.png',num:4,price:19,selected:true},
+          {id:2,title:'水果捞 100g',image:'/imgs/s6.png',num:1,price:9.9,selected:true}
+        ]
+      });
+      this.getTotalPrice();
+    },
+    /**
+     * 当前商品选中事件
+     */
+    selectList(e) {
+      const index = e.currentTarget.dataset.index;
+      let carts = this.data.carts;
+      const selected = carts[index].selected;
+      carts[index].selected = !selected;
+      this.setData({
+        carts: carts
+      });
+      this.getTotalPrice();
+    },
+  
+    /**
+     * 删除购物车当前商品
+     */
+    deleteList(e) {
+      const index = e.currentTarget.dataset.index;
+      let carts = this.data.carts;
+      carts.splice(index,1);
+      this.setData({
+        carts: carts
+      });
+      if(!carts.length){
+        this.setData({
+          hasList: false
+        });
+      }else{
+        this.getTotalPrice();
+      }
+    },
+  
