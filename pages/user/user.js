3d455bfdea85448bc983a1ce0766c88380781140
1610037483 <1610037483@qq.com>
2024-04-20 19:39:35 +0800

    lcx

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,66 +1,53 @@
-// pages/user/user.js
+// page/component/new-pages/user/user.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+    data:{
+      thumb:'',
+      nickname:'',
+      orders:[],
+      hasAddress:false,
+      address:{}
+    },
+    onLoad(){
+      var self = this;
+      /**
+       * 获取用户信息
+       */
+      wx.getUserInfo({
+        success: function(res){
+          self.setData({
+            thumb: res.userInfo.avatarUrl,
+            nickname: res.userInfo.nickName
+          })
+        }
+      }),
+  
+      /**
+       * 发起请求获取订单列表信息
+       */
+      wx.request({
+        url: 'http://www.gdfengshuo.com/api/wx/orders.txt',
+        success(res){
+          self.setData({
+            orders: res.data
+          })
+        }
+      })
+    },
+    onShow(){
+      var self = this;
+      /**
+       * 获取本地缓存 地址信息
+       */
+      wx.getStorage({
+        key: 'address',
+        success: function(res){
+          self.setData({
+            hasAddress: true,
+            address: res.data
+          })
+        }
+      })
+    },
+    /**
+     * 发起支付请求
+     */
